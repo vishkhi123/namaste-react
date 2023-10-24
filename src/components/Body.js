109a338597c9928c136f7bc0835f0fffc3630179
vishkhi123 <vishalkhiratkar@gmail.com>
2023-10-24 12:12:46 +0530

    day5 useState explained

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,28 @@
+import { useState } from "react"
 import RestroCard from "./RestroCard"
 import resList from "./utils/mockData"
 
 const Body=()=>{
+
+const [listOfRestautants,setListOfRestaurants]=useState(resList);
+
+
+
     return(
             <div className="body">
-                    <div className="Search">
-                            Search
+                    <div className="filter">
+                            <button className="filter-btn"
+                            onClick={()=>{
+                                const filterdList=listOfRestautants.filter((res)=>res.data.avgRating>4);
+                                setListOfRestaurants(filterdList);
+                            }}
+                            
+                            
+                             >Top Rated Restaurant</button>
                     </div>
                     <div className="restro-container">
                             
-                         {resList.map((rest)=>(
+                         {listOfRestautants.map((rest)=>(
                             <RestroCard key={rest.data.id} resName={rest}></RestroCard>
                          ))}  
                             
