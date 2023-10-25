b44b816da3dc3a1df157542d6934de068445baf7
vishkhi123 <vishalkhiratkar@gmail.com>
2023-10-25 22:03:40 +0530

    day6 updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,9 +5,9 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestautants, setListOfRestaurants] = useState([]);
-  const [filteredList,setFilteredList]=useState([]);
+  const [filteredList, setFilteredList] = useState([]);
 
-  const [searchBar,setSearchBar]=useState("");
+  const [searchBar, setSearchBar] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -23,11 +23,13 @@ const Body = () => {
     console.log("fetched Successfull");
     console.log(json);
     setListOfRestaurants(
+      json?.data?.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle
+        ?.restaurants
+    );
+    setFilteredList(
       json?.data.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle
         ?.restaurants
     );
-    setFilteredList( json?.data.success?.cards[1]?.gridWidget?.gridElements?.infoWithStyle
-        ?.restaurants)
   };
 
   if (listOfRestautants.length === 0) {
@@ -42,26 +44,28 @@ const Body = () => {
     <div className="body">
       <div className="filter">
         <div className="search">
-          <input type="text" className="search-bar" onChange={(e)=>setSearchBar(e.target.value)} value={searchBar}></input>
+          <input
+            type="text"
+            className="search-bar"
+            onChange={(e) => setSearchBar(e.target.value)}
+            value={searchBar}
+          ></input>
 
-        
-
-          <button  className="search-btn" onClick={()=>{
-               const filteredList= listOfRestautants.filter((e)=>(
-                        e.info.name.includes(searchBar)
-
-                       
-                        
-               ))
-                console.log(searchBar);
-                console.log(filteredList)
-                setListOfRestaurants(filteredList);
-          }}>
+          <button
+            className="search-btn"
+            onClick={() => {
+              const filteredList = listOfRestautants.filter((e) =>
+                e.info.name.toLowerCase().includes(searchBar.toLowerCase())
+              );
+              console.log(searchBar);
+              console.log(filteredList);
+              setFilteredList(filteredList);
+            }}
+          >
             Search
-
           </button>
         </div>
-       
+
         <button
           className="filter-btn"
           onClick={() => {
