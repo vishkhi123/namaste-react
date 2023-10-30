6d0e7a108f48603fd6def0370bcac5faf5c3f902
vishkhi123 <vishalkhiratkar@gmail.com>
2023-10-30 16:09:52 +0530

    day 8 on

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -10,14 +10,39 @@ class UserClass extends React.Component {
             count2:12,
             count3:0,
             count4:0,
+            userInfo:{
+                name:"Dummy",
+                location:"Default",
+            }
         }
+    }
 
+    async componentDidMount()
+    {
+        const data=await fetch("https://api.github.com/users/vishkhi123");
+        const json=await data.json();
+       console.log("Fetched Successfull")
+        console.log(json);
+
+        this.setState({
+            userInfo:json,
+        });
     }
 
 
   render() {
+
+    const{name,location,avatar_url}=this.state.userInfo;
+
     return (
       <div className="user-card">
+    <img src={avatar_url}></img>
+    <h3>Name : {name}</h3>
+    <h3>Location : {location}</h3>
+
+
+      <hr></hr>
+      
         <h2>Name :{this.props.name}</h2>
         <h3>Location:Dehradun</h3>
         <h4>Contact:@akshaymarch7</h4>
