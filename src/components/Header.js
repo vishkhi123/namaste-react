66cb02ef852673f431ba29838a418853b205123e
vishkhi123 <vishalkhiratkar@gmail.com>
2023-11-03 20:46:06 +0530

    day10 changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,19 +7,19 @@ const Header=()=>{
         const [btnName,setBtnName]=useState("Login");
 
     return(
-            <div className="header">
+            <div className="header flex justify-between bg-pink-100 shadow-2xl m-5">
                     <div className="logo-container">
-                            <img className="logo" src={LOGO_URL}/>
+                            <img className="logo w-52" src={LOGO_URL}/>
                     </div>
-                    <div className="nav-items">
-                            <ul>
+                    <div className="nav-items flex">
+                            <ul className="flex p-4 m-4">
                                     <li> <Link to="/">Home</Link></li>
                                     <li><Link to="/about">About Us</Link></li>
                                     <li><Link to="/contact">Contact</Link></li>
                                     <li>Cart</li>
-                                    <button className="login" onClick={()=>{
+                                    <li><button className="login" onClick={()=>{
                                         btnName==="Login"?setBtnName("Logoff"):setBtnName("Login")
-                                    }} >{btnName}</button>
+                                    }} >{btnName}</button></li>
                             </ul>
                     </div>
                     
